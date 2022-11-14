6d7f06f3aabea630ca99fb4e8d6a1c9ab4b1a2b9
bruce <xuzhongyuan@aliyun.com>
2022-11-14 18:40:17 +0800

    add cab menu

---
+++ src/api/testmanagement/cab.js
@@ -0,0 +1,65 @@
+import request from '@/utils/request'
+import {parseStrEmpty} from "@/utils/ruoyi";
+
+// 查询cab列表
+export function listCab(query) {
+    return request({
+        url: '/testmanagement/cab/list',
+        method: 'get',
+        params: query
+    })
+}
+
+// 查询cab详细
+export function getCab(cabId) {
+    return request({
+        url: '/testmanagement/cab/' + parseStrEmpty(cabId),
+        method: 'get'
+    })
+}
+
+
+// 新增cab
+export function addCab(data) {
+    return request({
+        url: '/testmanagement/cab',
+        method: 'post',
+        data: data
+    })
+}
+
+// 修改cab
+export function updateCab(data) {
+    return request({
+        url: '/testmanagement/cab',
+        method: 'put',
+        data: data
+    })
+}
+
+// 删除cab
+export function delCab(cabId) {
+    return request({
+        url: '/testmanagement/cab/' + cabId,
+        method: 'delete'
+    })
+}
+
+
+// 修改cab状态
+export function changeCabStatus(cabId, status) {
+    const data = {
+        userId,
+        status
+    }
+    return request({
+        url: '/testmanagement/cab/changeStatus',
+        method: 'put',
+        data: data
+    })
+}
+
+
+
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,7 @@ export const constantRoutes = [
     children: [
       {
         path: '/redirect/:path(.*)',
-        component: () => import('@/views/redirect/index.vue')
+        component: () => import('@/views/redirect/index-bak.vue')
       }
     ]
   },
